0a00f3b5ac727b486fcdb0abf1c1ae663c8fae17
K I T <kitilonom@gmail.com>
2019-01-04 19:14:14 +0300

    отчетыТ_Т

--- QueueLib/Queue.h
+++ QueueLib/Queue.h
@@ -1,3 +1,94 @@
 #pragma once
+#include <iostream>
+#include "ost.h"
 
+using namespace std;
+
+template <class T>
+class _Queue {
+	_Elem* head, *end; // указатели на начало и конец очереди
+	const unsigned max_size; // максимальный размер стека
+	unsigned now_size; // размер стека в данный момент времени
+
+public:
+	_Queue(unsigned _max_size = 0);	// конструктор по умолчанию	
+	~_Queue();
+	void push(T elem);	// кладем новую переменную в конец
+	T pull(); // достаем элемент с начала
+	bool is_empty(); // пуст ==  true
+	friend ofstream& operator<< (ofstream& ostr, _Queue& Qu); // перегрузка вывода
+
+};
+ 
+
+template <class T> // конструктор по умолчанию	
+_Queue<T>::_Queue(unsigned _max_size = 0)
+{
+	if (_max_size == 0) throw 1;
+	head = NULL;
+	end = NULL;
+	max_size = _max_size;
+	now_size = 0;
+}
+
+template <class T>
+_Queue<T>::~_Queue()
+{
+	_Elem* tmp = head;
+	while (tmp != NULL)
+	{
+		tmp->head->next; // запись адреса след адреса
+		delete head; // удаление ячейки
+		head = tmp; // сдвиг на след адрес
+	}
+}
+
+template <class T> // кладем новую переменную в конец
+void _Queue<T>::push(T elem)
+{
+	if (++now_size >= max_size) throw 2;
+	_Elem* em = new _Elem;
+	em->data = elem;
+	em->next = NULL;
+	if (head == NULL)
+	{
+		head = end = em;
+	}
+	else
+	{
+		end->next = em;
+		end = em;
+	}
+}
+ 
+template <class T> // достаем элемент с начала
+T _Queue<T>::pull()
+{
+	if (now_size == 0) throw 3;
+	T t = head->data;
+	_Elem em = head;
+	head = head->next;
+	delete em;
+	return t;
+}
+
+template <class T> // пуст ==  true
+bool _Queue<T>::is_empty()
+{
+	if (now_size == 0)
+		return true;
+	return false;
+}
+
+template <class T> // перегрузка вывода
+ofstream& operator<< (ofstream& ostr, _Queue<T>& Qu)
+{
+	_Elem* em = head;
+	while (em != NULL)
+	{
+		ostr << em->data;
+		em = em->next;
+	}
+	return ostr;
+}
 
--- StackLib/Stack.h
+++ StackLib/Stack.h
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-// стек представлен на односв€зном списке. простите :)
 // структура —“≈  - LIFO (англ. last in Ч first out, Ђпоследним пришЄл Ч первым вышелї).
 
 template <class T>
@@ -15,7 +14,7 @@ class _Stack {
 
 public:
 	_Stack(unsigned _max_size = 0);	// конструктор по умолчанию	
-	//_Stack(_Stack<T> &S); // конструктор копировани€
+	~_Stack();
 	void push(T elem);	// кладем новую переменную в стек
 	T pull(); // достаем элемент с вершиины
 	bool is_empty(); // пуст ==  true
@@ -31,27 +30,22 @@ _Stack<T>::_Stack(unsigned _max_size = 0)
 	now_size = 0;
 }
 
-//template <class T> // конструктор копировани€
-//_Stack<T>::_Stack(_Stack<T>& St)
-//{
-//	max_size = St.max_size;
-//	now_size = St.now_size;
-//	if (now_size == 0)
-//		top = NULL;
-//	else 
-//	{
-//		_Elem* em = 
-//		for (int i = 0; i < now_size; i++)
-//		{
-//
-//		}
-//	}
-//}
+template <class T> // деструкторо
+_Stack<T> ::~_Stack()
+{
+	_Elem* tmp = top;
+	while (tmp != NULL)
+	{
+		tmp->top->next; // запись адреса след адреса
+		delete top; // удаление €чейки
+		top = tmp; // сдвиг на след адрес
+	}
+}
 
 template <class T> // кладем новую переменную в стек
 void _Stack<T>::push(T elem)
 {
-	if (++now_size > max_size) throw 2;
+	if (++now_size >= max_size) throw 2;
 	_Elem *em = mew _Elem(); // создаем €чейку
 	em->data = elem; // записываем переменную в €чейку
 	if (now_size == 1) // если стек пуст был, то
@@ -67,10 +61,12 @@ template <class T> // достаем элемент с вершиины
 T _Stack<T>::pull()
 {
 	if (now_size == 0) throw 3;	// если пуст, то нельз€ что-то вз€ть
+	T t = top->data;
 	_Elem* tmp = *top; // сохран€ем во временную адрес вершины
 	*top = top->next; // ставим на вершину следующую
 	delete tmp; // удал€ем бывшую вершину
 	now_size--; // уменьшаем размер на единицу
+	return t;
 }
 
 template <class T> // пуст ==  true
